6d09afa6a4a60c733fe7a514e2a821dc0f2c01e7
Daniel Jordi Perez <danipq26@gmail.com>
2018-01-04 18:21:24 +0100

    refactor: refactored test to avoid duplications and put more explicit names

--- src/test/java/com/codurance/socialnetwork/SocialNetworkShould.java
+++ src/test/java/com/codurance/socialnetwork/SocialNetworkShould.java
@@ -5,6 +5,7 @@ import com.codurance.socialnetwork.domain.post.Posts;
 import com.codurance.socialnetwork.domain.user.Users;
 import com.codurance.socialnetwork.infrastructure.Clock;
 import com.codurance.socialnetwork.infrastructure.Console;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -17,6 +18,16 @@ import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SocialNetworkShould {
+
+    private static final String ALICE_MESSAGE = "I love the weather today";
+    private static final String ALICE_USERNAME = "Alice";
+    private static final LocalDateTime ALICE_POST_DATE = LocalDateTime.of(2018, 1, 1, 0, 0);
+    private static final String EXIT_COMMAND = "exit";
+    private static final String CHARLIE_USERNAME = "Charlie";
+    private static final String ALICE_POST_PRINTED = "I love the weather today (5 minutes ago)";
+    private static final String ALICE_POST_COMMAND = "Alice -> I love the weather today";
+    private static final Post ALICE_POST = new Post(ALICE_MESSAGE, ALICE_USERNAME, ALICE_POST_DATE);
+
     @Mock
     private Console console;
 
@@ -29,16 +40,20 @@ public class SocialNetworkShould {
     @Mock
     private Users userRepository;
 
-    private Post ALICE_POST = new Post("I love the weather today", "Alice", LocalDateTime.of(2018, 1, 1, 0, 0));;
+    private SocialNetwork socialNetwork;
+
+    @Before
+    public void setUp() {
+        socialNetwork = new SocialNetwork(console, clock, postRepository, userRepository);
+    }
 
     @Test
     public void
     save_a_post_when_receive_post_command() {
         given(console.readLine())
-                .willReturn("Alice -> I love the weather today")
-                .willReturn("exit");
+                .willReturn(ALICE_POST_COMMAND)
+                .willReturn(EXIT_COMMAND);
 
-        SocialNetwork socialNetwork = new SocialNetwork(console, clock, postRepository, userRepository);
         socialNetwork.run();
 
         verify(postRepository).save(ALICE_POST);
@@ -48,18 +63,16 @@ public class SocialNetworkShould {
     public void
     list_a_user_timeline_when_receive_the_username() {
         given(console.readLine())
-                .willReturn("Alice -> I love the weather today")
-                .willReturn("Alice")
-                .willReturn("exit");
+                .willReturn(ALICE_POST_COMMAND)
+                .willReturn(ALICE_USERNAME)
+                .willReturn(EXIT_COMMAND);
 
         given(clock.now())
-                .willReturn(LocalDateTime.of(2018, 1, 1, 0, 5));
-
-        SocialNetwork socialNetwork = new SocialNetwork(console, clock, postRepository, userRepository);
+                .willReturn(ALICE_POST_DATE);
 
         socialNetwork.run();
 
-        verify(console).printLine("I love the weather today (5 minutes ago)");
+        verify(console).printLine(ALICE_POST_PRINTED);
     }
 
     @Test
@@ -67,12 +80,10 @@ public class SocialNetworkShould {
     save_the_new_follower_of_a_user() {
         given(console.readLine())
                 .willReturn("Charlie follows Alice")
-                .willReturn("exit");
-
-        SocialNetwork socialNetwork = new SocialNetwork(console, clock, postRepository, userRepository);
+                .willReturn(EXIT_COMMAND);
 
         socialNetwork.run();
 
-        verify(userRepository).addFollower("Charlie", "Alice");
+        verify(userRepository).addFollower(CHARLIE_USERNAME, ALICE_USERNAME);
     }
 }

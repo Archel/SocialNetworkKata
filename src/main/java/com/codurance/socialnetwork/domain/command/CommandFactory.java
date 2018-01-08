7eb2fd2273208afd51df3fb1de73e42761343d65
Daniel Jordi Perez <danipq26@gmail.com>
2018-01-08 17:18:35 +0100

    green: Command Factory should return a FollowCommand

--- src/main/java/com/codurance/socialnetwork/domain/command/CommandFactory.java
+++ src/main/java/com/codurance/socialnetwork/domain/command/CommandFactory.java
@@ -1,6 +1,7 @@
 package com.codurance.socialnetwork.domain.command;
 
 import com.codurance.socialnetwork.domain.post.Posts;
+import com.codurance.socialnetwork.domain.user.Users;
 import com.codurance.socialnetwork.infrastructure.Clock;
 import com.codurance.socialnetwork.infrastructure.Console;
 
@@ -10,16 +11,23 @@ public class CommandFactory {
     private final Clock clock;
     private final Console console;
     private final Posts postRepository;
+    private final Users userRepository;
 
-    public CommandFactory(Clock clock, Console console, Posts postRepository) {
+    public CommandFactory(Clock clock, Console console, Posts postRepository, Users userRepository) {
         this.clock = clock;
         this.console = console;
         this.postRepository = postRepository;
+        this.userRepository = userRepository;
     }
 
     public Command create(String input) {
         if (Pattern.matches("^[A-z]+", input)) {
             return new DisplayTimeLineCommand(console);
+        }
+
+        if (Pattern.matches("^[A-z]+ follows [A-z]+", input)) {
+            String[] commandDetails = input.split(" follows ");
+            return new FollowUserCommand(userRepository, commandDetails[0], commandDetails[1]);
         }
 
         String[] commandDetails = input.split(" -> ");
--- src/test/java/com/codurance/socialnetwork/domain/command/CommandFactoryShould.java
+++ src/test/java/com/codurance/socialnetwork/domain/command/CommandFactoryShould.java
@@ -2,6 +2,7 @@ package com.codurance.socialnetwork.domain.command;
 
 
 import com.codurance.socialnetwork.domain.post.Posts;
+import com.codurance.socialnetwork.domain.user.Users;
 import com.codurance.socialnetwork.infrastructure.Clock;
 import com.codurance.socialnetwork.infrastructure.Console;
 import org.junit.Test;
@@ -25,10 +26,13 @@ public class CommandFactoryShould {
     private
     Posts postRepository;
 
+    @Mock
+    Users userRepository;
+
     @Test
     public void
     returns_a_post_command_when_receives_the_post_command_represented_as_string() {
-        CommandFactory commandFactory = new CommandFactory(clock, console, postRepository);
+        CommandFactory commandFactory = new CommandFactory(clock, console, postRepository, userRepository);
         Command postCommand = commandFactory.create("Alice -> Hello world!");
 
         assertThat(postCommand, instanceOf(PostCommand.class));
@@ -37,9 +41,18 @@ public class CommandFactoryShould {
     @Test
     public void
     returns_a_display_timeline_command_when_receives_the_display_timeline_command_represented_as_string() {
-        CommandFactory commandFactory = new CommandFactory(clock, console, postRepository);
+        CommandFactory commandFactory = new CommandFactory(clock, console, postRepository, userRepository);
         Command displayTimeLineCommand = commandFactory.create("Alice");
 
         assertThat(displayTimeLineCommand, instanceOf(DisplayTimeLineCommand.class));
+    }
+
+    @Test
+    public void
+    returns_a_follow_user_command_when_receives_the_follow_user_command_represented_as_string() {
+        CommandFactory commandFactory = new CommandFactory(clock, console, postRepository, userRepository);
+        Command followUserCommand = commandFactory.create("Charlie follows Alice");
+
+        assertThat(followUserCommand, instanceOf(FollowUserCommand.class));
     }
 }

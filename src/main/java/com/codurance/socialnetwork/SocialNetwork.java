c737fb8341c74eeb3f0860dbefdbf4f99eaa9855
Daniel Jordi Perez <danipq26@gmail.com>
2018-01-08 17:35:25 +0100

    refactor: changed switch statment smell to CommandFactory

--- src/main/java/com/codurance/socialnetwork/SocialNetwork.java
+++ src/main/java/com/codurance/socialnetwork/SocialNetwork.java
@@ -1,8 +1,6 @@
 package com.codurance.socialnetwork;
 
-import com.codurance.socialnetwork.domain.command.DisplayTimeLineCommand;
-import com.codurance.socialnetwork.domain.command.FollowUserCommand;
-import com.codurance.socialnetwork.domain.command.PostCommand;
+import com.codurance.socialnetwork.domain.command.*;
 import com.codurance.socialnetwork.domain.post.Posts;
 import com.codurance.socialnetwork.domain.user.Users;
 import com.codurance.socialnetwork.infrastructure.Clock;
@@ -34,19 +32,13 @@ public class SocialNetwork {
     }
 
     private void executeCommand(String inputCommand) {
-        if(Pattern.matches("^[A-z]+ -> [\\w\\s]+", inputCommand)) {
-            PostCommand postCommand = new PostCommand(postRepository, "I love the weather today", "Alice", LocalDateTime.of(2018, 1, 1, 0, 0));
-            postCommand.execute();
-        }
-
-        if (Pattern.matches("^[A-z]+", inputCommand)) {
-            DisplayTimeLineCommand displayTimeLineCommand = new DisplayTimeLineCommand(console);
-            displayTimeLineCommand.execute();
-        }
-
-        if (Pattern.matches("^[A-z]+ follows [A-z]+", inputCommand)) {
-            FollowUserCommand followUserCommand = new FollowUserCommand(userRepository, "Charlie", "Alice");
-            followUserCommand.execute();
+        CommandFactory commandFactory = new CommandFactory(clock, console, postRepository, userRepository);
+        
+        try {
+            Command command = commandFactory.create(inputCommand);
+            command.execute();
+        } catch (InvalidCommandException e) {
+            console.printLine("Invalid command.");
         }
     }
 }
